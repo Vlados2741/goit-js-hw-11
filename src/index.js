3516f3ab8a5ccd31e208096b7b7db7bf319306d7
Vlados2741 <ramosinho3@gmail.com>
2022-08-19 10:06:44 +0300

    v2

--- src/index.js
+++ src/index.js
@@ -2,53 +2,77 @@ import './css/styles.css';
 import axios from 'axios'
 import Notiflix from 'notiflix';
 const API_KEY = '29230090-341e91ba352ddd311dc4a279b';
-// const refs = {
-//     form: document.querySelector(`.search-form`),
-//     input: document.querySelector(`input`),
-//     button: document.querySelector(`button`),
-//     gallery: document.querySelector(`.gallery`)
-// };
+const refs = {
+    form: document.querySelector(`.search-form`),
+    input: document.querySelector(`input`),
+    button: document.querySelector(`button`),
+    gallery: document.querySelector(`.gallery`),
+    loadMore: document.querySelector(`.load-more`)
+};
 
-// refs.form.addEventListener("submit", getImages);
+refs.form.addEventListener("submit", searchSubmit);
+// refs.form.addEventListener("click", onLoadMore);
 
-// function searchSubmit(e) {
-//     e.preventDefault();
-//     const searchRequest = e.target.firstElementChild.value;
-//     getImages(searchRequest);
-// };
+function searchSubmit(e) {
+    e.preventDefault();
+    const searchRequest = e.target.firstElementChild.value;
+    getImages(searchRequest);
+};
 
-const getImages = async () => {
+const getImages = async (request) => {
     try {
         const response = await axios.get(
-            `https://pixabay.com/api/?key=29230090-341e91ba352ddd311dc4a279b&q=cat&image_type=photo`
+            `https://pixabay.com/api/?key=${API_KEY}&q=${request}&image_type=photo&per_page=40&orientation=horizontal&safesearch=true`
         );
         console.log(response.data);
+        if (response.data.total === 0) {
+            Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.")
+        } else {
+            Notiflix.Notify.success("Hooray! We found totalHits images.")
+            renderImagesList(response.data.hits)
+        }    
     } catch (error) {
         console.log(error)
     }
 };
-getImages()
-// function renderImagesList(images) {
-//         const markup = images
-//             .map((image) => {
-//                 return `
-//         <div class="photo-card">
-//             <img src="" alt="" loading="lazy" />
-//         <div class="info">
-//             <p class="info-item">
-//             <b>Likes</b>
-//             </p>
-//             <p class="info-item">
-//             <b>Views</b>
-//             </p>
-//             <p class="info-item">
-//             <b>Comments</b>
-//             </p>
-//             <p class="info-item">
-//             <b>Downloads</b>
-//             </p>
-//         </div>
-//         </div>
-//       `;}).join("");
-//         refs.gallery.innerHTML = markup;
-// };
+function renderImagesList(images) {
+        const markup = images
+            .map((image) => {
+                return `
+        <div class="photo-card">
+            <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" width="220px" height="150px"/>
+            <div class="info">
+                <p class="info-item">
+                <b>Likes:</b> <br>${image.likes}
+                </p>
+                <p class="info-item">
+                <b>Views:</b> <br>${image.views}
+                </p>
+                <p class="info-item">
+                <b>Comments:</b> <br>${image.comments}
+                </p>
+                <p class="info-item">
+                <b>Downloads:</b> <br>${image.downloads}
+                </p>
+            </div>
+        </div>
+      `;}).join("");
+        refs.gallery.innerHTML = markup;
+};
+// const onLoadMore = async (e) => {
+//     try {
+//         const response = await axios.get(
+//             `https://pixabay.com/api/?key=${API_KEY}&q=${request}&image_type=photo&per_page=40&orientation=horizontal&safesearch=true`
+//         );
+//         console.log(response.data);
+//         if (response.data.total === 0) {
+//             Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.")
+//         } else {
+//             Notiflix.Notify.success("Hooray! We found totalHits images.")
+//             renderImagesList(response.data.hits)
+//         }    
+//     } catch (error) {
+//         console.log(error)
+//     }
+
+// }
